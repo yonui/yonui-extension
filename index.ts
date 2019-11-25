b109c791b057a0965c25e8274bf8dbca9a934a9c
Yudu Ban <banyudu@gmail.com>
2019-11-25 11:34:28 +0800

    chore: add index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-export class Component {
+import { ReactNode } from 'react'
 
+export interface ComponentManifest { // 单个组件类型定义
+  label: string
+}
+
+export interface Manifest { // 组件包类型定义
+  components: ComponentManifest[]
+}
+
+export interface Component<P = any> {
+  manifest: ComponentManifest,
+  render: (props: P) => ReactNode
 }
