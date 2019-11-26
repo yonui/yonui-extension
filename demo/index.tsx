4b0636e1872be087548529f69f5461dbc8d2c824
ludl3 <ludl3@yonyou.com>
2019-11-26 10:26:05 +0800

    fix: update-demo

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,9 +1,50 @@
 import { Component } from '../index'
-
-export class CustomComponent implements Component {
+import { MetaTypes } from '../types/MetaTypes';
+// demo
+export class ButtonComponent implements Component {
   manifest = {
-    name: 'custom',
-    label: '自定义'
+    name: 'button',
+    label: '按钮',
+    description: '',
+    props: [
+      {
+        name: 'code',
+        type: String,
+        defaultValue: '',
+        showDesign: true,
+        designConfig: {
+          type: MetaTypes.Text,
+          isRequired: true,
+          props: {},
+          label: '编码',
+          labelStyle: JSON.stringify({color: 'red'}),
+          help: '这是编码字段'
+        }
+      },
+      {
+        name: 'type',
+        type: String,
+        defaultValue: '',
+        showDesign: true,
+        designConfig: {
+          type: MetaTypes.Select,
+          props:{
+            options:
+            [
+              { value: "primary", text: '主按钮' },
+              { value: "ghost", text: '透明' },
+              { value: "warning", text: '警告红' }
+            ]
+          },
+          label: '类型',
+          help:''
+        }
+      }
+    ],
+    children: [],
+    parent: function (parentNode:object) {
+      return true
+    }
   }
   render = () => ''
 }
--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
-import {  MetaTypes } from './types/MetaTypes';
+import { MetaTypes } from './types/MetaTypes';
 /**
  * designConfig props的描述
- * type  数据类型  适配层的可选视图类型(枚举项)
- * label 文本 string类型 在适配层页面显示的文本
+ * type  适配层的可选视图类型(枚举项)
+ * label 文本 在适配层页面显示的文本
  * labelStyle  在适配层页面显示的文本的样式文件
  * isRequired 是否必填
  * props 根据适配器中MetaTypes配置不同的props
@@ -13,21 +13,23 @@ interface designConfigProps {
   type: MetaTypes
   label: string
   labelStyle?: string
-  isRequired: boolean
+  isRequired?: boolean
   help?: string
   props: object
 }
 
 /**
  * 设计器适配层(Adapter)props描述
- * name 名称 string类型  必填
- * defaultValue 默认值--可以是数据,也可以是方法 非必填
+ * name 名称  
+ * type 数据类型
+ * defaultValue 默认值--可以是数据,也可以是方法 
  * showDesign 是否展示在适配层 当showDesign为false,需要配置designConfig字段
  * designConfig 适配层相关配置属性对象
  * change 当值改变时,是否会触发其它字段联动,值为
  */
 interface Props {
   name: string
+  type: any,
   defaultValue?: string
   showDesign: boolean
   designConfig?: designConfigProps
@@ -36,12 +38,12 @@ interface Props {
 
 /**
  * 自定义组件描述
- * name 组件名称,必填
- * label 组件文本,必填
- * description 组件的相关描述，非必填
- * props 设计器适配层(Adapter)需要的一些关键信息,非必填,如果不指定props，则使用默认配置，默认配置可随 type 变化
- * children 当前组件可放置哪些子组件,非必填
- * parent 当前组件可以放置哪些父组件，非必填
+ * name 组件名称
+ * label 组件文本
+ * description 组件的相关描述
+ * props 设计器适配层(Adapter)需要的一些关键信息
+ * children 当前组件可放置哪些子组件
+ * parent 当前组件可以放置哪些父组件
  */
 export interface ComponentManifest { // 单个组件类型定义
   name: string
@@ -59,41 +61,4 @@ export interface Manifest { // 组件包类型定义
 export interface Component<P = any> {
   manifest: ComponentManifest
   render: (props?: P, engine?: (props: any) => ReactNode) => ReactNode
-}
-
-// demo
-export class CustomComponent implements Component {
-  render = () => ''
-  manifest = {
-    name: 'cusom',
-    label: '自定义',
-    description: '',
-    props: [{
-      name: 'bHidden',
-      type: 'string',
-      defaultValue: '',
-      showDesign: true,
-      designConfig: {
-        type: MetaTypes.Select,
-        isRequired: true,
-        props:{
-          options:
-          [
-            {value: "top", text: '头部'},
-            {value: "batch", text: '批量'},
-            {value: "inrow", text: '在行中'}
-          ]
-        },
-        label: '',
-        labelStyle: JSON.stringify({color:'red'}),
-        help:''
-      }
-    }],
-    children:function (childNode:object) {
-      return true;
-    },
-    parent:function (parentNode:object) {
-      return true
-    }
-  }
 }
