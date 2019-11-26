450363e318d5818049f763f983e6e135289fb73e
ludl3 <ludl3@yonyou.com>
2019-11-26 17:08:51 +0800

    fix: 补全demo代码和注释内容

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 import { MetaTypes } from './types/MetaTypes';
 /**
- * designConfig props的描述
+ * designConfig props的描述 designConfigProps
  * type  适配层的可选视图类型(枚举项)
  * label 文本 在适配层页面显示的文本
  * labelStyle  在适配层页面显示的文本的样式文件
@@ -19,25 +19,31 @@ interface designConfigProps {
 }
 
 /**
- * 设计器适配层(Adapter)props描述
+ * 设计器适配层(Adapter)Props描述 Props
  * name 名称
  * type 数据类型
  * defaultValue 默认值--可以是数据,也可以是方法
+ * value 值
  * showDesign 是否展示在适配层 当showDesign为false,需要配置designConfig字段
  * designConfig 适配层相关配置属性对象
- * change 当值改变时,是否会触发其它字段联动,值为
+ * onChange 当值改变时,触发事件联动的方法,可改变其它配置字段的值
+ * onChange方法参数详解
+ * @param props 当前组件所有的props
+ * @param propsName 当前组件当前字段的propsName
+ * @param value 当前组件当前字段的值
  */
 export interface Props {
   name: string
   type: any
   defaultValue?: string
+  value?: string
   showDesign: boolean
   designConfig?: designConfigProps
-  onChange?: (props:Props[], propsName: string, value: string) => Props[]
+  onChange?: (props: Props[], propsName: string, value: string) => Props[]
 }
 
 /**
- * 自定义组件描述
+ * 自定义组件描述 ComponentManifest
  * name 组件名称
  * label 组件文本
  * description 组件的相关描述
