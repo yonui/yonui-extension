581edff94c2753c5562ca7942110aa5f29d1c80e
Yudu Ban <banyudu@gmail.com>
2019-12-02 11:28:17 +0800

    feat: support render children in wrapper

--- src/wrappers/react.tsx
+++ src/wrappers/react.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Wrapper, WrapperChild, WrapperResult } from '../types/wrapper'
 
+const renderChildren = (engine: any, children: any): any => {
+  if (Array.isArray(children)) {
+    return children.map(engine)
+  }
+  return engine(children)
+}
+
 const wrapper: Wrapper = (orig: WrapperChild): WrapperResult => {
   return (props: any, engine?: any) => {
-    const { nid, uitype } = props || {}
+    const { nid, uitype, children } = props || {}
     const divProps = { nid, uitype }
     return <div {...divProps}>
-      {orig(props, engine)}
+      {orig({ ...props, children: children ? renderChildren(engine, children) : undefined }, engine)}
     </div>
   }
 }
