04c1c2cf99d349a0f7495ca19164bf9925a59e3c
Yudu Ban <banyudu@gmail.com>
2019-12-04 14:42:37 +0800

    refactor(wrapper): refactor react wrapper

--- src/wrappers/react.tsx
+++ src/wrappers/react.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Wrapper, WrapperChild, WrapperResult } from '../types/wrapper'
+import { ComponentManifest } from '../types'
 
 const renderChildren = (engine: any, children: any): any => {
   if (Array.isArray(children)) {
@@ -8,12 +9,31 @@ const renderChildren = (engine: any, children: any): any => {
   return engine(children)
 }
 
-const wrapper: Wrapper = (orig: WrapperChild): WrapperResult => {
+function parseProps (props: any, engine: any, manifest?: ComponentManifest): any {
+  // 处理props，包括处理 children，以及将一些props做类型转换
+  if (props.children) {
+    props.children = renderChildren(engine, props.children)
+  }
+  return props
+}
+
+export interface WrapOptions {
+  excludeNidAndUiType?: boolean
+  manifest?: ComponentManifest
+}
+
+const wrapper: Wrapper = (orig: WrapperChild, options: WrapOptions = {}): WrapperResult => {
   return (props: any, engine?: any) => {
-    const { nid, uitype, children } = props || {}
+    const { nid, uitype } = props || {}
     const divProps = { nid, uitype }
+    const { excludeNidAndUiType, manifest } = options
+    const newProps = parseProps(props, engine, manifest)
+    const innerComp = orig(newProps, engine)
+    if (excludeNidAndUiType) {
+      return innerComp
+    }
     return <div {...divProps}>
-      {orig({ ...props, children: children ? renderChildren(engine, children) : undefined }, engine)}
+      {innerComp}
     </div>
   }
 }
