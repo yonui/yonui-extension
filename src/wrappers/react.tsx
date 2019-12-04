a9e8e396a1e876aefa1774d86f816a58a32a7ca8
Yudu Ban <banyudu@gmail.com>
2019-12-04 15:35:13 +0800

    feat: convert propTypes in react wrapper

--- src/wrappers/react.tsx
+++ src/wrappers/react.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Wrapper, WrapperChild, WrapperResult } from '../types/wrapper'
-import { ComponentManifest } from '../types'
+import { ComponentManifest, FieldTypes } from '../types'
 
 const renderChildren = (engine: any, children: any): any => {
   if (Array.isArray(children)) {
@@ -9,10 +9,37 @@ const renderChildren = (engine: any, children: any): any => {
   return engine(children)
 }
 
-function parseProps (props: any, engine: any, manifest?: ComponentManifest): any {
+function parseProps (origProps: any, engine: any, manifest?: ComponentManifest): any {
+  const props = { ...origProps }
   // 处理props，包括处理 children，以及将一些props做类型转换
   if (props.children) {
     props.children = renderChildren(engine, props.children)
+  }
+
+  // 根据manifest处理其它字段转换
+  if (manifest) {
+    const propsKeySet = new Set(Object.keys(props))
+    for (const propsItem of manifest.props || []) {
+      const { name, type } = propsItem
+      if (!propsKeySet.has(propsItem.name)) {
+        continue
+      }
+      if (type === FieldTypes.action) {
+        // Action(实际是String) => 函数
+        props[name] = (...args: any[]) => props.__dispatch__ ? props.__dispatch__(props[name], ...args) : console.log(`Action: ${props[name]}`)
+      } else if (type === FieldTypes.date) {
+        // String => Date
+        props[name] = new Date(props[name])
+      } else if (type === FieldTypes.array || type === FieldTypes.object) {
+        // String => JSON.parse 处理后的object
+        try {
+          props[name] = JSON.parse(props[name])
+        } catch (error) {
+          console.error(error)
+          props[name] = null
+        }
+      }
+    }
   }
   return props
 }
